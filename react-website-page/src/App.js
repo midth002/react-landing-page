b123eba5da041ebe6d4709e86cf7d9419340e62d
Andrew Midthun <midth002@gmail.com>
2022-05-28 14:59:32 -0500

    Added react router and Link to the navbar component.

--- react-website-page/src/App.js
+++ react-website-page/src/App.js
@@ -1,10 +1,19 @@
-// import './App.css';
+import './App.css';
+
+import Navbar from "./components/navbar/Navbar";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-     <h1>Hello world!</h1>
-    </div>
+    <>
+    <Router>
+    <Navbar />
+    <Routes>
+      <Route path='/' exact />
+    </Routes>
+    </Router>
+    </>
   );
 }
 
